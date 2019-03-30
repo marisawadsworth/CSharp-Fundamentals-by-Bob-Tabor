65f7af95e7cedf17d6c74eb909c1dea2fcac99e4
Marisa Wadsworth <marisawadsworth@Marisas-Air.hub>
2019-03-30 20:57:26 +1300

    lesson 8 Done

---
+++ 8_Defining_and_Calling_Methods/Method.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace _8_Defining_and_Calling_Methods
+{
+    public class Method
+    {
+        public Method()
+        {
+            //Methods help organise the code better, eliminate duplicate code, you can copy something and paste it later in the code.
+            //Gives a feature or functionality a name and call it by its name anywhere in the application.
+            //When you need to update you only need to go to that one place instead of the whole code.
+
+            HelloWorld();
+            Console.ReadLine();
+        }
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}
---
+++ 8_Defining_and_Calling_Methods/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace _8_Defining_and_Calling_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Name Game");
+
+            Console.Write("What's your first name? ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("What's your last name? ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("What city where you born in? ");
+            string city = Console.ReadLine();
+
+            /*
+            char[] firstNameArray = firstName.ToCharArray();
+            Array.Reverse(firstNameArray);
+
+            char[] lastNameArray = lastName.ToCharArray();
+            Array.Reverse(lastNameArray);
+
+            char[] cityArray = city.ToCharArray();
+            Array.Reverse(cityArray);
+
+            string result = " ";
+
+            foreach (char item in firstNameArray)
+            {
+                result += item;
+            }
+
+            result += " ";
+
+            foreach (char item in lastNameArray)
+            {
+                result += item;
+            }
+
+            result += " ";
+
+            foreach (char item in cityArray)
+            {
+                result += item;
+            }
+
+            Console.WriteLine("Results: " + result);
+
+            Console.ReadLine();
+            */
+
+
+            DisplayResult(
+                ResverseString(firstName),
+                ResverseString(lastName),
+                ResverseString(city));
+
+        }
+
+        private static string ResverseString(string message)
+        {
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+            return String.Concat(messageArray);
+            //Concat - Allows to pass in an array of individual characters and return them as a full string.
+
+            /*
+            foreach (char item in messageArray)
+            {
+                Console.Write(item);
+            }
+             Console.Write(" ");
+             */
+        }
+
+        private static void DisplayResult(
+            string reversedFirstName,
+            string reversedLastName,
+            string reversedCity)
+        {
+            Console.Write("Results: ");
+            Console.Write(String.Format("{0} {1} {2}",
+                reversedFirstName,
+                reversedLastName,
+                reversedCity));
+        }
+
+    }
+}
+
