e4000c4cd524acf25b2432e6f530b8a686df6718
Marisa Wadsworth <marisawadsworth@Marisas-Air.hub>
2019-04-20 15:21:43 +1200

    Lesson 5 Done

--- 5_Operators_Expressions_and_Statements/Program.cs
+++ 5_Operators_Expressions_and_Statements/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections;
 
 namespace _5_Operators_Expressions_and_Statements
 {
