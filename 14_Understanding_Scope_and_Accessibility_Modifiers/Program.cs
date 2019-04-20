044a80f5a0691aabc5754eb589e72460a11da925
Marisa Wadsworth <marisawadsworth@Marisas-Air.hub>
2019-04-20 17:16:35 +1200

    Lesson 14 Done

--- 14_Understanding_Scope_and_Accessibility_Modifiers/Program.cs
+++ 14_Understanding_Scope_and_Accessibility_Modifiers/Program.cs
@@ -8,9 +8,53 @@ namespace _14_Understanding_Scope_and_Accessibility_Modifiers
 {
     class Program
     {
+        private static string k = "";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string j = "";
+
+            for (int i = 0; i < 10; i++)
+            {
+                j = i.ToString();
+                k = i.ToString();
+                Console.WriteLine(i);
+
+                if (i == 9)
+                {
+                    string l = i.ToString();
+                }
+                //Console.WriteLine(l);
+
+            }
+            //Can access i outside of the for loop with j and k.
+            //Console.WriteLine(i);
+            Console.WriteLine("Outside of the for: " + j);
+            Console.WriteLine("Outside of the for: " + k);
+
+            HelperMethod();
+
+            Car myCar = new Car();
+            myCar.DoSomething();
+
+            Console.ReadLine();
+        }
+
+        static void HelperMethod()
+        {
+            Console.WriteLine("Value of k from the HelperMethod(): " + k);
+        }
+    }
+
+    class Car
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine(helperMethod());
+        }
+        private string helperMethod()
+        {
+            return "Hello World!";
         }
     }
 }
