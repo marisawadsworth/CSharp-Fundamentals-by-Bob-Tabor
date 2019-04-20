4cfbc68b6659030e65593ec6bcfd9faf7634222d
Marisa Wadsworth <marisawadsworth@Marisas-Air.hub>
2019-04-20 17:40:00 +1200

    Lesson 15 Done

--- 15_Understanding_Namespace_and_Working_with_the_.Net_Class_Library/Program.cs
+++ 15_Understanding_Namespace_and_Working_with_the_.Net_Class_Library/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
 
 namespace _15_Understanding_Namespace_and_Working_with_the_.Net_Class_Library
 {
@@ -10,7 +9,18 @@ namespace _15_Understanding_Namespace_and_Working_with_the_.Net_Class_Library
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Looks up the microsoft website and downloads the code in the termial and on the computer.
+
+            //Needs the using System.Net; to work.
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://msdn.microsoft.com");
+
+            //Dont need System as it is mentioned at the top.
+            System.Console.WriteLine(reply);
+            //Needs the using System.IO; to work. Saves the file onto the computer.
+            File.WriteAllText(@"C:\Lesson17\WriteTxt.txt", reply);
+            Console.ReadLine();
+
         }
     }
 }
