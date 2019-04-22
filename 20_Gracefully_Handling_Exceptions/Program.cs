f7ceb0861ac52da33d6f0c5b7c12cfa3cacc5787
Marisa Wadsworth <marisawadsworth@Marisas-Air.hub>
2019-04-23 09:36:33 +1200

    Lesson 20 Done

--- 20_Gracefully_Handling_Exceptions/Program.cs
+++ 20_Gracefully_Handling_Exceptions/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +12,39 @@ namespace _20_Gracefully_Handling_Exceptions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                string content = File.ReadAllText(@"Example.txt");
+                Console.WriteLine(content);
+                Console.ReadLine();
+            }
+
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the name of the file is named correctly.");
+            }
+
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the name of the directory is named correctly.");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                //Code to finalize.
+                //Setting objects to null.
+                //Closing database connections.
+                Console.WriteLine("Closing application now ...");
+            }
+            Console.ReadLine();
         }
     }
 }
